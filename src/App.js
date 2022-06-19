aa7eb5f69c22052945524bbee782674e20daf5cb
reddevill007 <shivampandey2900@gmail.com>
2022-06-19 19:37:15 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-    return <h1>hii</h1>;
+    return (
+        <Switch>
+            <Route exact path="/">
+                this is home page
+            </Route>
+
+            <Route exact path="/starred">
+                this is starred page
+            </Route>
+
+            <Route>404 not found</Route>
+        </Switch>
+    );
 }
 
 export default App;
