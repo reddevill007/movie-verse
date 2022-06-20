31dcbead4eebdaf2683b7d3228932fe4c7ecc7fb
reddevill007 <shivampandey2900@gmail.com>
2022-06-20 09:14:46 +0530

    added useReducer instead of useState on the show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,26 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { useParams } from 'react-router-dom'
 import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS': {
+            return { isLoading: false, error: null, show: action.show }
+        }
+
+        case 'FETCH_FILED': {
+            return { ...prevState, isLoading: false, error: action.error }
+        }
+
+        default: return prevState;
+    }
+}
+
+const initialState = {
+    show: null,
+    isLoading: true,
+    error: null
+}
+
 
 const Show = () => {
     const { id } = useParams();
 
-    const [show, setShow] = useState(null);
-    const [isLoading, setisLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [{ show, isLoading, error }, dispatch] = useReducer(reducer, initialState);
 
     useEffect(() => {
         let isMounted = true;
 
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
             if (isMounted) {
-                setShow(results);
-                setisLoading(false);
+                dispatch({ type: 'FETCH_SUCCESS', show: results })
             }
         }).catch(err => {
             if (isMounted) {
-                setError(err.message);
-                setisLoading(false);
+                dispatch({ type: 'FETCH_FILED', error: err.message })
             }
         })
 
