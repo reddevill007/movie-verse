f6249cf556022611724bfbf0011f21579597a6a8
reddevill007 <shivampandey2900@gmail.com>
2022-06-20 15:25:57 +0530

    custom hooks created

---
+++ src/misc/custom-hooks.js
@@ -0,0 +1,33 @@
+import { act } from 'react-dom/test-utils';
+import { useReducer, useEffect } from 'reacts';
+
+function showReducer(prevState, action) {
+    switch (action.type) {
+        case 'ADD': {
+            return [...prevState, action.showId];
+        }
+
+        case 'REMOVE': {
+            return prevState.filter(showId => showId !== action.showId);
+        }
+        default:
+            return prevState;
+    }
+}
+
+function usePersistedReducer(reducer, initialState, key) {
+    const [state, dispatch] = useReducer(reducer, initialState, initial => {
+        const persisted = localStorage.getItem(key);
+        return persisted ? JSON.parse(persisted) : initial;
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state));
+    }, [state, key]);
+
+    return [state, dispatch];
+}
+
+export function useShows(key = 'shows') {
+    return usePersistedReducer(showReducer, [], key);
+}
